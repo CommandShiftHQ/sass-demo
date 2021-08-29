17f63b4dae725995be0a7528ffdd298b40de9264
StuartCowley <voidstalker@live.co.uk>
2021-08-29 13:46:36 +0100

    install node-sass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import './scss/headers.scss'
 
 function App() {
   return (
     <div className="App">
-      React
+      <div className="text--bold">Demo component</div>
+      <h1>h1</h1>
+      <h2>h2</h2>
+      <h3>h3</h3>
+      <h4>h4</h4>
+      <h5>h5</h5>
+      <h6>h6</h6>
     </div>
   );
 }
